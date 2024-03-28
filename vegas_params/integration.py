01f31d5051104cef571f073fc2740cc6d2a08be4
Andrey Sheshukov <dxdydz2@gmail.com>
2024-03-28 14:12:55 +0300

    Using the factor in the integration

--- vegas_params/integration.py
+++ vegas_params/integration.py
@@ -1,9 +1,23 @@
 import vegas
 from .base import Expression
+from warnings import warn
+import numpy as np
+from loguru import logger
 
 def integral(e: Expression):
     """decorator for turning expression into integral"""
     integrator = vegas.Integrator(e.input_limits)
+    @vegas.lbatchintegrand
+    def _integrand(x):
+        result = e(x)
+        if isinstance(result, np.ndarray):
+            return np.squeeze(result) * np.squeeze(e.factor)
+        elif isinstance(result, dict):
+            return {key:np.squeeze(value)*np.squeeze(e.factor) for key,value in result.items()}
+        else:
+            logger.warn('Failed to apply the factor to result of type {}',type(result))
+            return result
+        
     def _run(**vegas_parameters):
-        return integrator(vegas.lbatchintegrand(e), **vegas_parameters)
+        return integrator(_integrand, **vegas_parameters)
     return _run
