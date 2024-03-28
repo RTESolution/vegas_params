328a17e41d13de088140f838ad7ec097a0e93e8d
Andrey Sheshukov <dxdydz2@gmail.com>
2024-03-28 23:03:36 +0300

    Adding test

--- test/test_integrals.py
+++ test/test_integrals.py
@@ -3,7 +3,7 @@ from vegas_params import Uniform, Fixed
 import numpy as np
 
 def assert_close(x, value, nsigma=1):
-    assert np.abs(x.mean-value)<x.sdev*nsigma
+    assert np.abs(x.mean-value) < x.sdev*nsigma
 
 def test_1d_constant():
     @integral
@@ -11,6 +11,13 @@ def test_1d_constant():
     def e(x):
         return np.ones(x.shape[0])
 
-    assert_close(e(),5)
+    assert_close(e(nitn=50), 5)
 
+def test_Gaussian():
+    @integral
+    @expression(x=Uniform([-100,100]))
+    def e(x):
+        return np.exp(- x**2)
+
+    assert_close(e(nitn=50), np.sqrt(np.pi))
 
