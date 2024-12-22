16735a53caea3dd2db6e0288a17244ebf4bb08ab
Andrey Sheshukov <ash@jinr.ru>
2024-12-22 12:58:40 +0300

    Adding 'sample_with_factor' method

--- src/base.py
+++ src/base.py
@@ -22,6 +22,49 @@ class Parameter(abc.ABC):
                               self.input_limits[:,1], 
                               size=[size,len(self)])
         return self.__construct__(x)
+        
+    def sample_with_factor(self, N=1, iter_max=10):
+        """Generate a random sample of this expression values, 
+        taking into account the factor as the probability density.
+
+        Note: this process is iterative (it might need to generate several samples), and several times slower than regular :meth:`sample` method
+        """
+        #start with the sample of size N
+        the_sample = self.sample(0) #just get the output size
+        the_factor = self.factor
+        the_random = np.empty_like(the_factor)
+        selected = []
+        N_generate = int(N*2) #the size of next sample to generate
+        for it in range(iter_max):
+            sample, factor = self.sample(N_generate), self.factor
+            if it==0:
+                if np.min(self.factor)==np.max(self.factor):
+                    #the factor is equal for all values, so just return this sample
+                    return sample[:N]
+                
+            factor*=np.ones(len(sample)) #make sure the factor is array of size len(sample)
+            random = np.random.uniform(size=len(sample))
+            the_sample = np.append(the_sample, sample, axis=0)
+            the_factor = np.append(the_factor, factor, axis=0)
+            the_random = np.append(the_random, random, axis=0)
+            #apply the selection
+            selected = (the_random*the_factor.max()) < the_factor
+            N_selected = selected.sum()
+            print(f"Iteration #{it}: generated {N_generate} -> selected={N_selected}/{len(the_factor)}")
+            if N_selected>=N:
+                return the_sample[selected][:N]
+            else:
+                prob = N_selected/len(the_sample) #estimated selection probability
+                #now decide how many items to generate
+                # N = prob*(N_total) = N_selected + prob*N_generate =>
+                N_generate = (N-N_selected)/prob
+                #additional 20% to avoid making small iterations
+                N_generate *= 1.2
+                #make sure we don't make a sample too small or too large
+                N_generate = np.clip(N_generate, N/100, N*1000)
+                N_generate = np.asarray(np.ceil(N_generate), dtype=int)
+        raise RuntimeError(f"Maximum iterations ({iter_max}) reached. Generated only {sum(selected)} points of {N} requested")
+
 
 class Fixed(Parameter):
     """Fixed value, not a part of integration"""
