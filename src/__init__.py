e12c8b35e5e31ae97a77f09c1b01675530577311
Andrey Sheshukov <ash@jinr.ru>
2024-12-29 09:26:36 +0300

    Bump version

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 from . import base, vector, collections, sampling
 from .base import Uniform, Fixed, FromDistribution, Expression, expression
 from .vector import Direction, Vector, Scalar
