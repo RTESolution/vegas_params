8fc5517ae8c57916603352c08712eb0a5075ce86
Andrey Sheshukov <dxdydz2@gmail.com>
2025-03-18 12:50:38 +0300

    Update tests - check the normalized_integral utility

--- test/test_integrals.py
+++ test/test_integrals.py
@@ -1,37 +1,29 @@
 import numpy as np
-from vegas_params import expression, Uniform, Vector, Direction, Scalar, vector
-from vegas_params import integral
+import vegas_params as vp
 
 def assert_integral_is_close(e, value, precision=0.1):
-    x = integral(e)(nitn=30, neval=10000)
+    x = vp.integral(e)(nitn=30, neval=10000)
     assert np.abs(x.mean - value) < value*precision
     
 def test_1d_constant_integral():
     #test linear integral
-    @expression(x=Uniform([0,5]))
+    @vp.expression(x=vp.Uniform([0,5]))
     def constant(x):
         return np.ones(x.shape[0])
 
     assert_integral_is_close(constant,5)
 
 def test_2d_constant_integral():
-    @expression(x=Uniform([0,5]), y=Uniform([0,5]))
+    @vp.expression(x=vp.Uniform([0,5]), y=vp.Uniform([0,5]))
     def constant(x,y):
         return np.ones(x.shape[0])
 
     assert_integral_is_close(constant,25)
 
 def test_Gaussian_integral():
-    @expression(x=Uniform([-100,100]))
-    def gaussian(x):
-        return np.exp(-x**2)
-
-    assert_integral_is_close(gaussian, np.sqrt(np.pi))
-
-def test_Gaussian_integral():
     #test linear integral with factor
     
-    @expression(x=Uniform([-100,100]))
+    @vp.expression(x=vp.Uniform([-100,100]))
     def gaussian(x):
         return np.exp(-x**2)
 
@@ -42,7 +34,7 @@ def test_Spherical_integral_simple():
         return np.ones(r.shape[0])
     #test spherical integral with factor
     Rsphere=10
-    @expression(R=Scalar(Uniform([0,Rsphere])), direction=Direction())
+    @vp.expression(R=vp.Scalar(vp.Uniform([0,Rsphere])), direction=vp.Direction())
     def density(R, direction):
         r = R*direction
         return R**2
@@ -51,18 +43,38 @@ def test_Spherical_integral_simple():
 
 def test_Spherical_integral():
     #test spherical integral with factor
-    @expression
+    @vp.expression
     class Spherical:
-        R:Scalar = Scalar(Uniform([0,1]))
-        s:Direction = Direction()
+        R:vp.Scalar = vp.Scalar(vp.Uniform([0,1]))
+        s:vp.Direction = vp.Direction()
         def __call__(self,R,s):
             self.factor = R**2
             return R*s
 
     Rsphere=10
     
-    @expression(r=Spherical(R=Scalar(Uniform([0,Rsphere]))))
-    def density(r:vector):
+    @vp.expression(r=Spherical(R=vp.Scalar(vp.Uniform([0,Rsphere]))))
+    def density(r:vp.Vector):
         return np.ones(r.shape[0])
         
     assert_integral_is_close(density, 4/3*np.pi*Rsphere**3)
+
+def test_Spherical_integral_normalized():
+    #test spherical integral with factor
+    @vp.expression
+    class Spherical:
+        R:vp.Scalar = vp.Scalar(vp.Uniform([0,1]))
+        s:vp.Direction = vp.Direction()
+        def __call__(self,R,s):
+            self.factor = R**2
+            return R*s
+
+    Rsphere=10
+
+    @vp.utils.normalize_integral(123)
+    @vp.expression(r=Spherical(R=vp.Scalar(vp.Uniform([0,Rsphere]))))
+    def density(r:vp.Vector):
+        return np.ones(r.shape[0])
+        
+    assert_integral_is_close(density, 123)
+
