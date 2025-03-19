6916a9602f1b4cf3ae95ec40a0332ac6dc5eb3dd
Andrey Sheshukov <dxdydz2@gmail.com>
2025-03-19 14:30:37 +0300

    Allow integral to have a separate adapt expression

--- src/integration.py
+++ src/integration.py
@@ -16,28 +16,33 @@ def integral_naiive(e: Expression):
         result = np.prod(np.diff(e.input_limits)) * np.sum(wv, axis=0)/v.shape[0]
         return gvar.gvar(result.mean(axis=0),result.std(axis=0))
 
+def make_integrand(e: Expression):
+    def _integrand(x):
+            result = e.__construct__(x)
+            if isinstance(result, dict):
+                return {key:np.squeeze(value)*np.squeeze(e.factor) for key,value in result.items()}
+            else:
+                return np.squeeze(result) * np.squeeze(e.factor)
+    return _integrand
     
 def integral(e: Expression):
     """decorator for turning expression into integral"""
-    def _integrand(x):
-        result = e.__construct__(x)
-        if isinstance(result, dict):
-            return {key:np.squeeze(value)*np.squeeze(e.factor) for key,value in result.items()}
-        else:
-            return np.squeeze(result) * np.squeeze(e.factor)
-
     if(len(e)>0):
         integrator = vegas.Integrator(e.input_limits)
         def _run_integral(adapt=False, **vegas_parameters):
-            if adapt:
+            if adapt==True:
+                #adapt on the same function
+                adapt=e
+            if adapt!=False:
                 #run the calculation without storing the result
-                integrator(vegas.lbatchintegrand(_integrand), nitn=10, neval=1000)
-            return integrator(vegas.lbatchintegrand(_integrand), **vegas_parameters, adapt=False)
+                assert isinstance(adapt, Expression), f"adapt must be an Expression object, not {type(adapt)}"
+                integrator(vegas.lbatchintegrand(make_integrand(adapt)), nitn=10, neval=1000)
+            return integrator(vegas.lbatchintegrand(make_integrand(e)), **vegas_parameters, adapt=False)
         return _run_integral
     
     else:    
         def _just_calculate(**vegas_parameters):
-            return gvar(_integrand(np.empty(shape=(1,0))))
+            return gvar(make_integrand(e)(np.empty(shape=(1,0))))
         
         return _just_calculate
         
