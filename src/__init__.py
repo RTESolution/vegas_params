f015c74612e8feac142e7314b1a21d03fa4f24ab
Andrey Sheshukov <dxdydz2@gmail.com>
2025-04-28 12:05:43 +0300

    Bump minor version

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 from . import base, vector, collections, sampling, utils
 from .base import Uniform, Fixed, FromDistribution, Expression, expression
 from .vector import Direction, Vector, Scalar
